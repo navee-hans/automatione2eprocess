b17ff164e70bb2ad13efd2160ab7aea1004cacfb
navee-hans <naveehans@gmail.com>
2025-01-06 22:01:26 +0530

    updated code

--- src/test/java/AutomationSetup.java
+++ src/test/java/AutomationSetup.java
@@ -1,45 +1,75 @@
+import org.checkerframework.checker.units.qual.A;
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class AutomationSetup {
+    private static AutomationSetup instance;
+    private static ThreadLocal<WebDriver> driverManager = new ThreadLocal<>();
+    private static WebDriver driver;
 
-    public WebDriver driver;
+    private AutomationSetup(){}
 
-    @BeforeClass
-    public void startBrowser(){
-        driver = new ChromeDriver();
+    private void initBrowser(String browser) {
+        switch (browser) {
+            case "chrome":
+                driverManager.set(new ChromeDriver());
+                break;
+
+            default:
+                new IllegalArgumentException("invalid browser name: " + browser);
+
+        }
     }
 
+    public static AutomationSetup getInstance(String browser){
+        if(instance==null){
+            synchronized (AutomationSetup.class){
+                if(instance==null)
+                {
+                    instance = new AutomationSetup();
+                }
+            }
+        }
 
-    @Test
-    public void test3(){
+        if(driverManager.get()==null){
+            instance.initBrowser(browser);
+        }
+           return instance;
+        }
 
+        public WebDriver getDriver(){
+        return driverManager.get();
+        }
+
+        @BeforeMethod
+        public void setup(){
+        driver = AutomationSetup.getInstance("chrome").getDriver();
         driver.get("https://google.com");
-        String title = driver.getTitle();
-        Assert.assertEquals(title,"Google","title is invalid");
+        }
 
-    }
+        @Test
+        public void test1(){
+            Assert.assertEquals(driver.getTitle(), "Google","not found");
+        }
 
-    @Test
-    public void test1(){
-        driver.get("https://google.com");
-        String title = driver.getTitle();
-        Assert.assertEquals(title,"Google","title is invalid");
-    }
+        @Test
+        public void test2(){
+            Assert.assertEquals(driver.getTitle(), "Google","not found");
+        }
 
+        @AfterMethod
+        public void killdriver(){
+        if(driverManager.get()!=null)
+        {
+            driverManager.get().quit();
+            driverManager.remove();
+        }
 
-    @Test
-    public void test2(){
-        driver.get("https://google.com");
-        String title = driver.getTitle();
-        Assert.assertEquals(title,"Google","title is invalid");
-    }
-
-    @AfterClass
-    public void killChromeDriver(){
-        driver.quit();
-        driver = null;
-    }
+            }
 }
